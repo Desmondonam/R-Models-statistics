aee2ccd0899c3db81f3c54f9cf27eded192f2d54
Desmond Onam <desmondonam@gmail.com>
2022-04-07 22:00:17 +0300

    add data

---
+++ Question2.R
@@ -0,0 +1,19 @@
+## get the packages that we are working with. 
+library(wordcloud)
+library(e1071)
+library(gmodels)
+
+
+## Get the data that we are working with
+
+my_data <- read.delim("D:/Obita/21123456D/Assignment_Data/Q2/train_tweets_new.txt")
+my_data
+
+table(my_data)
+
+
+
+
+
+
+
