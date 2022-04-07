f780d871831afba0ac932eda60895674a6e4e728
Desmond Onam <desmondonam@gmail.com>
2022-04-07 23:28:04 +0300

    final

--- question3.R
+++ question3.R
@@ -19,3 +19,40 @@ head(data)
 
 # Structure 
 str(data)
+glimpse(data)
+
+summary(data)
+
+# Implementing the K-means algorithm
+
+
+
+# function to compute total within-cluster sum of square 
+wss <- function(k) {
+  kmeans(df, k, nstart = 10 )$tot.withinss
+}
+
+# Compute and plot wss for k = 1 to k = 15
+k.values <- 1:15
+
+# extract wss for 2-15 clusters
+wss_values <- map_dbl(k.values, wss)
+
+plot(k.values, wss_values,
+     type="b", pch = 19, frame = FALSE, 
+     xlab="Number of clusters K",
+     ylab="Total within-clusters sum of squares")
+
+kmean <- kmeans(data, 2)
+kmean$centers
+
+
+
+
+
+
+
+
+
+
+
